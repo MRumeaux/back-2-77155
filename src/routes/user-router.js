c2548d978bc417edd0d8829cb6135ac331933736
MRumeaux <mrumeaux@gmail.com>
2025-11-04 14:53:31 -0300

    ajuste

--- src/routes/user-router.js
+++ src/routes/user-router.js
@@ -7,27 +7,41 @@ const router = Router();
 // GET ALL
 router.get("/", async(req, res) =>{
     const users = await userModel.find();
-    res.status(200).send({ message: "Todos los usuarios" });
+    res.status(200).send({ users: users });
 })
 // GET BY ID
 router.get("/:id", async(req, res) =>{
     const { id } = req.params;
     const user = await userModel.findById(id);
-    res.status(200).send({ message: "Un usuario" });
+    res.status(200).send({ user: user });
 })
 // POST
-router.post("/", async(res, req) =>{
-    const {first_name, last_name, email} = req.body;
-    const newUser = await userModel.create({first_name, last_name, email})
-    res.status(201).send({ message: "Hola Coders", payload: newUser });
+router.post("/", async(req, res) =>{
+    res.status(201).send({ message: "Realizar registro en /register" });
 })
 // PUT
-router.put("/:id", async(res, req) =>{
-    res.status(201).send({ message: "Hola Coders" });
+router.put("/:id", async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const updatedUser = await userModel.findByIdAndUpdate(id, req.body, {new: true})
+        if (!updatedUser) return res.status(404).json({ message: "Usuario no fue actualizado" });
+        res.status(201).send({ message: "Usuario actualizado", user: updatedUser });
+    } catch (error) {
+        res.status(500).json({message: "Error actualizando usuario", err: error.message})
+    }
+
 })
 // DELETE
-router.delete("/:id", async(res, req) =>{
-    res.status(200).send({ message: "Hola Coders" });
+router.delete("/:id", async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const deletedUser = await userModel.findByIdAndDelete(id);
+        if (!deletedUser) return res.status(404).json({ message: "Usuario no fue eliminado" });
+        res.status(200).send({ message: "Usuario eliminado", user: deletedUser });
+    } catch (error) {
+        res.status(500).json({message: "Error eliminando usuario", err: error.message})
+    }
+
 })
 
 
